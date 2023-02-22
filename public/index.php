75b5996af4dff4fc2ee8cba8e73e4bcc13a49647
Phil OSullivan <phil.osullivan@gmail.com>
2023-02-22 14:20:04 -0500

    updates

--- public/index.php
+++ public/index.php
@@ -9,13 +9,13 @@ require __DIR__ . '/../vendor/autoload.php';
 Dotenv\Dotenv::createImmutable(dirname(__DIR__))->load();
 
 // .
-$app = require __DIR__ . '/../bootstrap/app.php';
+$app = require __DIR__ . '/../app/app.php';
 
 // .
-(require __DIR__ . '/../bootstrap/middleware.php')($app);
+(require __DIR__ . '/../app/middleware.php')( $app );
 
 // .
-(require __DIR__ . '/../bootstrap/routes.php')($app);
+(require __DIR__ . '/../app/routes.php')( $app );
 
 // .
 $app->run();
