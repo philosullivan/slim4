f19ecde806b5047568cd196ab16a979a7bcf6f42
Phil OSullivan <phil.osullivan@gmail.com>
2023-02-23 10:06:19 -0500

    clean up

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -1,16 +1,19 @@
 <?php
-
+// .
 declare(strict_types=1);
 
+// .
 namespace App\Controllers;
 
+// .
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-class ContactController extends Controller
-{
-    public function index(Request $request, Response $response): Response
-    {
-        return $this->render($response, 'contact/index.twig');
+// .
+class ContactController extends Controller {
+
+    // .
+    public function index( Request $request, Response $response ): Response {
+        return $this->render( $response, 'contact/index.twig' );
     }
 }
