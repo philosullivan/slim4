1dd4952336ef1686d9891e1b7011521ea3c0f681
Phil O'Sullivan <phil.osullivan@gmail.com>
2023-03-12 22:40:06 -0400

    tweaks

--- app/src/Controllers/HomeController.php
+++ app/src/Controllers/HomeController.php
@@ -15,11 +15,13 @@ class HomeController extends Controller {
 	public function index( Request $request, Response $response ): Response {
 		$this->logger->info('HomeController');
 
+		// Test Query.
 		$query = $this->connection->select()->from('test');
 		$rows = $query->execute()->fetchAll() ?: [];
-
 		$this->logger->info( print_r( $query, true ) );
 		$this->logger->info( print_r( $rows, true ) );
+
+
 		return $this->render( $response, 'home/index.twig' );
 	}
 }
